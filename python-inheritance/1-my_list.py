43f29d368901a669a02723c802f8a412e2f5e676
C4lice <laura.aupetit002@gmail.com>
2025-05-28 16:24:44 +0200

    changement de de self a self.copy

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -3,12 +3,14 @@
 module that create a square class
 """
 
-
 class MyList(list):
     """
     create a class MyList
     """
     def print_sorted(self):
-        print_list = self[:]
+        """
+        function print the list in ascending order
+        """
+        print_list = self.copy()
         print_list.sort()
         print(print_list)
