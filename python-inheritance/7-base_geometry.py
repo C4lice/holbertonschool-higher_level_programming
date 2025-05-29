abb9e21bfbd2f68c5132925f2e00f4421f39f9ba
C4lice <laura.aupetit002@gmail.com>
2025-05-29 23:02:52 +0200

    rectification du fichier txt 1-my_list.txt et fiechier 7-base_geometry.py et .txt

---
+++ python-inheritance/7-base_geometry.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+Module that creates a BaseGeometry class.
+"""
+
+
+class BaseGeometry:
+    """
+    Base class for geometry-related operations.
+    """
+
+    def area(self):
+        """
+        Raises:
+            Exception: Always raised to indicate that the method is not implemented.
+        """
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """
+        Validates that the value is a positive integer.
+
+        Args:
+            name (str): The name of the parameter (used in the error message).
+            value (int): The value to validate.
+
+        Raises:
+            TypeError: If value is not an integer.
+            ValueError: If value is less than or equal to 0.
+        """
+        if type(value) is not int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
