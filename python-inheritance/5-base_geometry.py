f31ba80758a4c345061e0e459dfd039b712cedc3
C4lice <laura.aupetit002@gmail.com>
2025-05-29 22:28:48 +0200

    fichier base_geometry.py

---
+++ python-inheritance/5-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+module that create a square class.
+"""
+
+
+class BaseGeometry:
+    """
+    An empty class used as a base for geometry-related classes.
+    """
+    pass
