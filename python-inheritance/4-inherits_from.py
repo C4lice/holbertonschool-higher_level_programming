e97d899c8aae696534d046cb1cc80ffccd36eff3
C4lice <laura.aupetit002@gmail.com>
2025-05-29 22:23:08 +0200

    mise a jours de la la docstring du fichier inherits_from.py

---
+++ python-inheritance/4-inherits_from.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+module that create a square class
+"""
+
+
+def inherits_from(obj, a_class):
+    """
+    Returns True if obj is an instance of a subclass of a_class (but not a_class itself), otherwise False.
+    """
+    return isinstance(obj, a_class) and type(obj) is not a_class
