24bc19d8e54d625f2e840f66705339715b58af36
C4lice <laura.aupetit002@gmail.com>
2025-05-30 19:22:03 +0200

    tests fichier avec pycodestyle

--- python-inheritance/3-is_kind_of_class.py
+++ python-inheritance/3-is_kind_of_class.py
@@ -6,6 +6,7 @@ module that create a square class
 
 def is_kind_of_class(obj, a_class):
     """
-    Returns True if obj is an instance of a_class or a subclass of a_class, otherwise False.
+    Returns True if obj is an instance of a_class
+    or a subclass of a_class, otherwise False.
     """
     return isinstance(obj, a_class)
