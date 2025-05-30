abaa37d29ebdd9a7903a899071218d16ca3cf42a
C4lice <laura.aupetit002@gmail.com>
2025-05-30 14:24:23 +0200

    fichier a testes

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -6,6 +6,7 @@ module that create a square class
 
 def inherits_from(obj, a_class):
     """
-    Returns True if obj is an instance of a subclass of a_class (but not a_class itself), otherwise False.
+    Returns True if obj is an instance of a subclass of a_class
+    (but not a_class itself), otherwise False.
     """
     return isinstance(obj, a_class) and type(obj) is not a_class
