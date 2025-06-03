493c9ad2910f1f108c4e643988f87ac9417e1832
C4lice <laura.aupetit002@gmail.com>
2025-06-03 14:54:07 +0200

    test avec modication nom et variable du fichier 0-read_file.py

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
 
+'''
+This the module contain function for read a file
+'''
+
 
 def read_file(filename=""):
     """reads a UTF-8 text file and displays it on stdout"""
-    with open(filename,"r", encoding='utf-8') as my_file:
-        print(my_file.read(), end="")
+    with open(filename,"r", encoding='UTF-8') as file:
+        print(file.read(), end="")
