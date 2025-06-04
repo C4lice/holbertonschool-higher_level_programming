f7fabe5c94a2a3e9847062b7ece2f3e2f41bc7eb
C4lice <laura.aupetit002@gmail.com>
2025-06-04 11:21:09 +0200

    envoye du fichier 3-to-json_string

---
+++ python-input_output/3-to_json_string.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+
+'''
+This module contain function that append string in text
+'''
+import json
+
+
+def to_json_string(my_obj):
+    '''
+    this function to append string in a file
+    '''
+    return json.dumps(my_obj)
