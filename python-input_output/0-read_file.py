7e3a659eb17d0d5118012635af59e7db8202a0bb
C4lice <laura.aupetit002@gmail.com>
2025-06-04 10:36:40 +0200

    file 0-read_file sans le end dans le print(file.read())

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -8,6 +8,6 @@ This the module contain function for read a file
 def read_file(filename=""):
 
     """reads a UTF-8 text file and displays it on stdout"""
-    
+
     with open(filename,"r", encoding='UTF-8') as file:
-        print(file.read(), end="")
+        print(file.read())
