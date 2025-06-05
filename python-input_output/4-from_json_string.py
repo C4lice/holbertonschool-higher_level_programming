0dcb03334121a591b8ca412eb381fc3623a6378c
C4lice <laura.aupetit002@gmail.com>
2025-06-05 09:09:31 +0200

    file 4-from_json_string

---
+++ python-input_output/4-from_json_string.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+'''
+This module contain function that append string in text
+'''
+
+
+import json
+
+
+def from_json_string(my_str):
+    '''
+    this function to append string in a file
+    '''
+    return json.loads(my_str)
