c4d5bb6bc5fd354842ac96762c1a2d08bdff53ae
C4lice <laura.aupetit002@gmail.com>
2025-06-05 09:10:10 +0200

    file 5-save_to_json_file

---
+++ python-input_output/5-save_to_json_file.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+'''
+This module contain function that append string in text
+'''
+
+
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    '''
+    this function to append string in a file
+    '''
+    with open(filename, 'w', encoding='UTF-8') as fichier:
+        return fichier.write(json.dumps(my_obj))
