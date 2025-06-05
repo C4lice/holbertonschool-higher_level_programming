e12f22ffe044b79510c169349ade0b23f1c133b0
C4lice <laura.aupetit002@gmail.com>
2025-06-05 09:11:26 +0200

    file 6-load_from_json

---
+++ python-input_output/6-load_from_json_file.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+'''
+This module contain function that append string in text
+'''
+
+
+import json
+
+
+def load_from_json_file(filename):
+    '''
+    this function to append string in a file
+    '''
+    with open(filename, 'r', encoding='UTF-8') as fichier:
+        return json.loads(fichier.read())
