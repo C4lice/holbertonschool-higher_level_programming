f555c2ab77120e3474c37408281ecc47ca48446b
C4lice <laura.aupetit002@gmail.com>
2025-06-27 11:31:07 +0200

    feat: create the file 7-cities

---
+++ SQL_more_queries/7-cities.sql
@@ -0,0 +1,10 @@
+-- script that create the database hbtn_0d_usa and the table cities (in the database hbtn_0d_usa
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities(
+    id INT NOT NULL AUTO_INCREMENT,
+    state_id INT NOT NULL,
+    name VARCHAR(256) NOT NULL,
+    PRIMARY KEY (id),
+    UNIQUE (id),
+    CONSTRAINT citie_in_state FOREIGN KEY (state_id) REFERENCES states(id)
+);
