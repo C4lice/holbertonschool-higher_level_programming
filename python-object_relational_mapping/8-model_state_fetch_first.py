e45544498d07b6bf1ac98fdb342cbedfe6ffab2d
C4lice <laura.aupetit002@gmail.com>
2025-06-27 23:02:18 +0200

    feat: create the file 8-model_state_fetch_first

---
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+Start link class to table in database
+"""
+import sys
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm.exc import NoResultFound
+
+
+def main():
+    """
+    fonction use to search good data
+    """
+    session = sessionmaker(engine)
+    session = session()
+    try:
+        state = session.query(State).order_by(State.id).limit(1).one()
+        print("{}: {}".format(state.id, state.name))
+    except NoResultFound:
+        print("Nothing")
+
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    main()
