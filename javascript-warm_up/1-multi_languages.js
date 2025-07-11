8c0d5be3b9c6e9d42ef4a8e9e67625bd2b257a9e
C4lice <laura.aupetit002@gmail.com>
2025-07-11 23:32:53 +0200

    feat: ajouter plusieurs fichiers JavaScript vides pour le projet

---
+++ javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+const firstv = 'C is fun';
+const seconv = 'Python is cool';
+const thirdv = 'JavaScript is amazing';
+
+console.log(firstv);
+console.log(seconv);
+console.log(thirdv);
