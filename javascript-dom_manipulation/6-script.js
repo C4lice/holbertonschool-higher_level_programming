4d8ade20fa38f4c8cdc034ae7ef89708735c2473
C4lice <laura.aupetit002@gmail.com>
2025-07-19 16:42:34 +0200

    feat: create the file 6-script.js

---
+++ javascript-dom_manipulation/6-script.js
@@ -0,0 +1,10 @@
+// Prend l'élément avec id 'character'
+const docs = document.getElementById('character');
+
+// Demande les infos du personnage
+fetch('https://swapi-api.hbtn.io/api/people/5/?format=json')
+  .then(response => response.json()) // Lis la réponse
+  .then(data => {
+    const name = document.createTextNode(data.name); // Prend son nom
+    docs.appendChild(name); // L’ajoute dans la page
+  });
